0cb6695980b330e4de746fa7ae328281b5f8e5f8
michalkral92 <michalkral92@gmail.com>
2025-04-03 21:05:53 +0200

    8. lekce - Page Object Model

---
+++ tests/learning/pmtool/page_objects_tests.spec.ts
@@ -0,0 +1,10 @@
+import { test } from "@playwright/test";
+import { LoginPage } from "../../../src/pages/pmtool/login_page.ts";
+
+test("Test Page Objects", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.openPmtool();
+  await loginPage.typeUsername("pw_academy");
+  await loginPage.typePassword("Playwright321!");
+  await loginPage.clickLogin();
+});
