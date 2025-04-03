abba5ee50e85342e3f269ece4105f34bb769e354
michalkral92 <michalkral92@gmail.com>
2025-04-03 19:29:06 +0200

    První test PW

---
+++ tests/learning/first-scripts/first_tests.spec.ts
@@ -0,0 +1,10 @@
+import { test } from "@playwright/test";
+
+// Pro zjednodušení můžeme použít zkratku pwt, která nabídne snipper se základní strukturou testu (nutno mít Playwright Test Snippets  rozšíření od M. Skeltona)
+test("První test", async ({ page }) => {
+  //Testovací kroky
+  await page.goto("https://tredgate.com/pmtool/");
+  await page.locator("#username").fill("pw_academy");
+  await page.locator("#password").fill("Playwright321!");
+  await page.locator(".btn").click();
+});
